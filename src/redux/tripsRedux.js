ecda87fab2f45f7e444cabc0a0225dfd59029ed9
Marta Koperek <makoperek@gmail.com>
2019-12-09 22:39:33 +0100

    Change getTripById, getTripsForCountry selectors

--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -21,16 +21,18 @@ export const getFilteredTrips = ({trips, filters}) => {
 };
 
 export const getTripById = ({trips}, tripId) => {
-  const filtered = trips;
-
   // TODO - filter trips by tripId
 
-  console.log('filtering trips by tripId:', tripId, filtered);
-  return filtered.length ? filtered[0] : {error: true};
+  const filtered = trips.filter(trip => trip.id === tripId);
+
+  console.log('Filtering trips by tripId: ', tripId, filtered);
+  return filtered.length===1 ? filtered[0] : {error: true};
+
+
 };
 
 export const getTripsForCountry = ({trips}, countryCode) => {
-  const filtered = trips;
+  const filtered = trips.filter(trip => trip.country.code === countryCode);
 
   // TODO - filter trips by countryCode
 
