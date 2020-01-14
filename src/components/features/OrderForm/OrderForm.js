e0b312b3722d2b4aefa05047adb7b5386bc13957
Marta Koperek <makoperek@gmail.com>
2020-01-14 22:09:55 +0100

    Add alert which prevents sending order without name and contact details

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -8,63 +8,78 @@ import Button from '../../common/Button/Button';
 import settings from '../../../data/settings.js';
 import {formatPrice} from '../../../utils/formatPrice';
 import {calculateTotal} from '../../../utils/calculateTotal';
+import styles from './OrderForm.scss';
 
 
+const sendOrder = (options, tripId, tripName, tripCost) => {
+  const {name, contact} = options;
 
-//import styles from './OrderForm.scss';
-
-
-const sendOrder = (options, tripCost, tripName, tripCountry, tripId) => {
   const totalCost = formatPrice(calculateTotal(tripCost, options));
 
   const payload = {
     ...options,
-    totalCost,
-    tripName,
     tripId,
-    tripCountry,
+    tripName,
+    totalCost,
   };
 
   const url = settings.db.url + '/' + settings.db.endpoint.orders;
 
-  const fetchOptions = {
-    cache: 'no-cache',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(payload),
-  };
+  if (name != '' && contact != '') {
+    const fetchOptions = {
+      cache: 'no-cache',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    };
 
-  fetch(url, fetchOptions)
-    .then(function(response){
-      return response.json();
-    }).then(function(parsedResponse){
-      console.log('parsedResponse', parsedResponse);
-    });
+    fetch(url, fetchOptions)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(parsedResponse) {
+        console.log('parsedResponse', parsedResponse);
+      });
+  } else {
+    alert('Please fill in the name and contact!');
+  }
 };
 
-const OrderForm = ({tripCost,setOrderOption, options, tripName, tripCountry, tripId}) => {
+const OrderForm = ({ tripCost, tripId, tripName, options, setOrderOption }) => {
+  console.log('OrderFromTHIS', tripId, tripName);
+  // setOrderOption={setOrderOption}
   return (
     <Row>
-      {pricing.map((option) => (
-        <Col md={4} key={option.id}>
-          <OrderOption {...option} currentValue={options[option.id]} setOrderOption={setOrderOption}/>
-        </Col>
-      ))}
+      {pricing.map(option => {
+        // const option = options.map(option => option.id);
+        return (
+          <Col className={styles.component} key={option.id} md={4}>
+            <OrderOption
+              setOrderOption={setOrderOption}
+              currentValue={options[option.id]}
+              {...option}
+            />
+          </Col>
+        );
+      })}
       <Col xs={12}>
-        <OrderSummary tripCost={tripCost} options={options}/>
+        <Button onClick={() => sendOrder(options, tripId, tripName, tripCost)}>
+          Order now!
+        </Button>
+        <OrderSummary tripCost={tripCost} options={options} />
       </Col>
-      <Button onClick={() => sendOrder(options, tripCost, tripName, tripCountry, tripId)}>Order now!</Button>
     </Row>
   );
 };
+
 OrderForm.propTypes = {
   tripCost: PropTypes.string,
   options: PropTypes.object,
   setOrderOption: PropTypes.func,
-  tripName: PropTypes.string,
   tripId: PropTypes.string,
-  tripCountry: PropTypes.string,
+  tripName: PropTypes.string,
 };
+
 export default OrderForm;
